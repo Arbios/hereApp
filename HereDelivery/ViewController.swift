c9c6fa087beec8350a4facf9432552fd2534e3e7
Arbi Bashaev <arbi.personal@gmail.com>
2019-03-22 21:44:29 +0300

    Initial commit, clean xCode project

---
+++ HereDelivery/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HereDelivery
+//
+//  Created by ARBI BASHAEV on 22/03/2019.
+//  Copyright © 2019 ARBI BASHAEV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
